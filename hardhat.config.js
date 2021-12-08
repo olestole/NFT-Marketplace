6875b290bf25d8db971bcae515faf4e441cf2ce6
Ole August <oleastole@gmail.com>
2021-12-08 16:09:10 +0100

    feat: hardhat setup

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [process.env.METAMASK_PRIVATE_KEY],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [process.env.METAMASK_PRIVATE_KEY],
+    },
+  },
+  solidity: "0.8.4",
+};
